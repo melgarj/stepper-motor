9d103c04565ad1f5df22ba1e694d106cd411cc26
jelucian <jlucian995@gmail.com>
2018-04-27 12:04:54 -0700

    changed to left button activation

--- stepper-motor/stepper.d
+++ stepper-motor/stepper.d
@@ -1,4 +1,4 @@
 .\stepper.o: stepper.c
-.\stepper.o: C:\Keil_v4\ARM\ARMCC\bin\..\include\stdint.h
+.\stepper.o: C:\Keil_v4.74\ARM\ARMCC\bin\..\include\stdint.h
 .\stepper.o: tm4c123gh6pm.h
 .\stepper.o: systick.h
--- stepper-motor/steppertestmain.d
+++ stepper-motor/steppertestmain.d
@@ -1,4 +1,4 @@
 .\steppertestmain.o: StepperTestMain.c
-.\steppertestmain.o: C:\Keil_v4\ARM\ARMCC\bin\..\include\stdint.h
+.\steppertestmain.o: C:\Keil_v4.74\ARM\ARMCC\bin\..\include\stdint.h
 .\steppertestmain.o: stepper.h
 .\steppertestmain.o: tm4c123gh6pm.h
